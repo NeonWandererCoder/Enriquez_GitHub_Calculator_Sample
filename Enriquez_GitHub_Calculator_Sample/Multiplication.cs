e71a0ca26b18ef419a1b036359a4f853682442e4
Ign-keed <ignacio.304913@balagtas.sti.edu.ph>
2026-02-11 20:25:40 +0800

    Feat add Multiplication

---
+++ Enriquez_GitHub_Calculator_Sample/Multiplication.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enriquez_GitHub_Calculator_Sample
+{
+    public class Multiplication
+    {
+        public static int Multiply(int num1, int num2) {
+            return num1 * num2;
+
+
+      }
+    
+    }
+}
